5e2a0af34316b4d89b27361864001a63e1739750
Andre Wester <wester@mediamus.de>
2025-10-03 13:53:29 +0200

    Refactor module structure and enhance functionality.
    Renamed files for better consistency, added new repeaters (slider, two columns) and fields to Matrix Basic, and improved rendering logic with file existence validation. Updated module icons and dependencies for enhanced clarity and accessibility.

--- classes/SeparatorHelper.php
+++ classes/SeparatorHelper.php
@@ -5,59 +5,75 @@ namespace ProcessWire\classes;
 class SeparatorHelper
 {
     /**
-     * function for the content_separator and maybe for the basic_separator
-     *
-     * @param string $matrix
-     * @param string $class
-     * @param string $mobile
-     * @param string $tablet
-     * @param string $desktop
-     * @param bool $separator
+     * @param $page
      *
      * @return string
      */
-    public function getSeparator(string $matrix, string $class, string  $mobile,string  $tablet,string  $desktop, bool $separator) : string
+    public function getSeparator($page) : string
     {
-        $html = '';
+        $css_class = $page->text_class;
+        $image = $page->image;
+        $options = $page->select_separator_options;
 
-        if ( $matrix == 'basic_separator' ) {
-            $tag = 'section';
-            $container = ' class="container"';
-        } else {
-            $tag = 'div';
-            $container = '';
+        switch ($options) {
+            case "1":
+                return $this->getSeparatorOffset($page, $css_class);
+            case "2":
+                return '<div class="position-relative separator-image">' . $this->getSeparatorImage($image, " left start-0") . '</div>';
+            case "3":
+                return '<div class="position-relative separator-image">' . $this->getSeparatorImage($image, " center") . '</div>';
+            case "4":
+                return '<div class="position-relative separator-image">' . $this->getSeparatorImage($image, " right end-0") . '</div>';
+            case "5":
+                return $this->getNextSectionArrow();
+            default:
+                return '';
         }
+    }
 
-        $c = ($class) ? ' ' . $class : '';
+    private function getSeparatorOffset($page, $css_class): string
+    {
+        $mobile = $page->text_difference_mobile;
+        $tablet = $page->text_difference_tablet;
+        $desktop = $page->text_difference_desktop;
+        $separator = $page->checkbox_separator;
+
+        $c = ($css_class) ? ' ' . $css_class : '';
         $m = ($mobile != '') ? ' value-mobile="' . $mobile . '"' : ' value-mobile="30"';
         $t = ($tablet != '') ? ' value-tablet="' . $tablet . '"' : ' value-tablet="50"';
 
         switch (true) {
             case $separator && $desktop != '' :
-                $html .= '
-                    <' . $tag . ' class="' . $matrix . $c . '" style="margin: ' . $desktop . 'px 0px;"' . $m . $t . ' data-aos="fade-up" data-aos-duration="1000">
-                        <div' . $container . '>
-                            <hr>
-                        </div>
-                    </' . $tag . '>';
-                break;
+                return '
+                    <div class="separator-settings' . $c . '" style="margin: ' . $desktop . 'px 0;"' . $m . $t . '>
+                        <hr>
+                    </div>';
             case $separator :
-                $html .= '
-                    <' . $tag . ' class="' . $matrix . $c . '" style="margin: 50px 0px"' . $m . $t . ' data-aos="fade-up" data-aos-duration="1000">
-                        <div' . $container . '>
-                            <hr>
-                        </div>
-                    </' . $tag . '>';
-                break;
+                return '
+                    <div class="separator-settings' . $c . '" style="margin: 50px 0"' . $m . $t . '>
+                        <hr>
+                    </div>';
             case $desktop != '' :
-                $html .= '
-                    <' . $tag . ' class="' . $matrix . $c . '" style="margin: ' . $desktop . 'px 0px;"' . $m . $t . ' data-aos="fade-up" data-aos-duration="1000"></' . $tag . '>';
-                break;
+                return '
+                    <div class="separator-settings' . $c . '" style="margin: ' . $desktop . 'px 0;"' . $m . $t . '></div>';
             default :
-                $html .= '
-                    <' . $tag . ' class="' . $matrix . $c . '" style="margin:  50px 0px; "' . $m . $t . ' data-aos="fade-up" data-aos-duration="1000"></' . $tag . '>';
+                return '<div class="separator-settings' . $c . '" style="margin:  50px 0; "' . $m . $t . '></div>';
         }
+    }
 
-        return $html;
+    private function getSeparatorImage($image, $css_class = '')
+    {
+        return '<img class="position-absolute top-50' . $css_class . '" src="' . $image->url . '" alt="' . $image->description . '">';
+    }
+
+    private function getNextSectionArrow(): string
+    {
+        return '
+            <div class="next-section-arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            </div>
+        ';
     }
 }
